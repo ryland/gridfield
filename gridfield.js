4ddbb4dc67509d0d8cbc9550c4480796daf6b8ce
Ry Wharton <ry@howcast.com>
2008-10-19 00:34:09 -0700

    Minor tweaks.

--- gridfield.js
+++ gridfield.js
@@ -13,17 +13,14 @@ $.fn.gridfield = function(options) {
   
   var self = $(this);
   var columns = [];
+  var columnCount = 0;
   var modules = self.children("li");
 
   modules.each(function(i,m) { m.w = Math.ceil($(m).width()/(settings.columnWidth+settings.gutter)) });
   var updateColumnCount = function() { 
-    settings.columnCount = Math.floor(($(window).width()-settings.gutter) / (settings.columnWidth + settings.gutter));
+    columnCount = Math.floor(($(window).width()-settings.gutter) / (settings.columnWidth + settings.gutter));
   }
   
-  $(window).resize(function() { 
-    arrange();
-  }); 
-
   var heightSort = function(a,b) { 
     if (a.y == b.y) return a.x-b.x; 
     else return a.y-b.y;
@@ -36,7 +33,7 @@ $.fn.gridfield = function(options) {
   arrange = function() {
     updateColumnCount();
     columns = [];
-    for (i=0; i<settings.columnCount;i++) {
+    for (i=0; i<columnCount;i++) {
       columns[i] = { x:i, y:0 };
     }
 
@@ -44,9 +41,9 @@ $.fn.gridfield = function(options) {
       var next;
       if (m.w == 1) {
         next = columns.sort(heightSort)[0];
-      } else if (m.w <= settings.columnCount){
+      } else if (m.w <= columnCount){
         var possibles = [];
-        $.each( columns.sort(leftToRightSort).slice(0,(settings.columnCount-m.w)+1) , function(i,c) {
+        $.each( columns.sort(leftToRightSort).slice(0,(columnCount-m.w)+1) , function(i,c) {
           // find minimum height to be placed in this column
           var max = -1;
           for(j=i; j<i+m.w; j++) {
@@ -61,11 +58,16 @@ $.fn.gridfield = function(options) {
       // update all spanned columns
       $(m).css({position:"absolute", top:next.y+"px", left:next.x*(settings.columnWidth+settings.gutter)+"px"}).show();
       $.each(columns.sort(leftToRightSort).slice(next.x,next.x+m.w), function(i,v) { v.y = next.y+$(m).height()+settings.yPadding; });
-      self.height(columns.sort(heightSort)[settings.columnCount-1].y);
-      self.width(settings.columnCount*(settings.columnWidth+settings.gutter)-settings.gutter);
+      self.css({ height: columns.sort(heightSort)[columnCount-1].y,
+                 width: columnCount*(settings.columnWidth+settings.gutter)-settings.gutter });
     });
     
   }
+
+  $(window).resize(function() { 
+    arrange();
+  }); 
+
   arrange();
-  return(self);
+  return self;
 }
